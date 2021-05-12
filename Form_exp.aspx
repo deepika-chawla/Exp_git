cb2f1e0fae7168028bbff9ce34f658d40966334f
SHOBHA CHAWLA <shobha29290@ascs.sies.edu.in>
2021-05-12 13:08:34 +0530

    Add project files.

---
+++ Form_exp.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form_exp.aspx.cs" Inherits="Gitexp_pro.Form_exp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Text="Enter name:"></asp:TextBox>
+            <asp:TextBox ID="TextBox2" runat="server" Text="Enter sur name:"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="submit" OnClick="Button1_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Form_exp.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gitexp_pro
+{
+    public partial class Form_exp : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-UKMR9CC;Initial Catalog=Gitdata;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd2 = new SqlCommand("insert into Data_exp(Name,Surname) values(@name,@sur)", conn);
+            cmd2.Parameters.AddWithValue("@name", TextBox1.Text);
+            cmd2.Parameters.AddWithValue("@sur", TextBox2.Text);
+            cmd2.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
